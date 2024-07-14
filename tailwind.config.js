1e7593a566e04426c299136b7701abd976381e10
ERNEST ANOKYE <44618031+iamernesto14@users.noreply.github.com>
2024-07-14 15:16:32 +0000

    adjusted image size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          'dark-blue': '#6d7f97',
+          'grayish-blue': '#9eafc2'
+        },
+        secondary: {
+          'dark-grayish-blue': '#48556a',
+          'light-grayish-blue': '#ecf2f8'
+        }
+      }
+    },
   },
   plugins: [],
 }
